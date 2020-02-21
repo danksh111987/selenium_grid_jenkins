6ca59eacf5c3203a2fd9971b90788567b1556a28
danksh111987 <danksh111987@gmail.com>
2020-02-21 23:11:09 +0530

    modified listner

--- src/test/java/com/orangehrm/common/ImplIinvokedListner.java
+++ src/test/java/com/orangehrm/common/ImplIinvokedListner.java
@@ -1,9 +1,14 @@
 package com.orangehrm.common;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
@@ -17,19 +22,33 @@ public class ImplIinvokedListner implements IInvokedMethodListener {
 		if(method.isTestMethod()){
 		//System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Chrome78ver\\chromedriver.exe");
 			//Browser version changed
-		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Chrome80ver\\chromedriver.exe");
-
-		WebDriverFactory.setWebDriver(new ChromeDriver());
+			
+		//System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Chrome80ver\\chromedriver.exe");
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("chrome");
+		capabilities.setVersion("80");
 		
-		//for extend reports
-		ExtentReportTestFactory.createNewTest(method);
+		try {
+			WebDriver dr = new RemoteWebDriver(new URL("http://192.168.1.6:8090/wd/hub/"), capabilities);
+			WebDriverFactory.setWebDriver(dr);
+			//for extend reports
+			ExtentReportTestFactory.createNewTest(method);
+			
+			// implicit wait setting
+			WebDriverFactory.getWebDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+			WebDriverFactory.getWebDriver().manage().window().maximize();
+
+			WebDriverFactory.getWebDriver().get("http://127.0.0.1/orangehrm-3.3.1/symfony/web/index.php/auth/login");
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		// implicit wait setting
-		WebDriverFactory.getWebDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		WebDriverFactory.getWebDriver().manage().window().maximize();
-
-		WebDriverFactory.getWebDriver().get("http://127.0.0.1/orangehrm-3.3.1/symfony/web/index.php/auth/login");
+		
+		
+		
 		}
 		
 	}
